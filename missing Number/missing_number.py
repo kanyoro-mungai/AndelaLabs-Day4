5c9129c51ff2a600d9a5341a4bd17857c0b6ac10
kanyoro-mungai <gmungai6@gmail.com>
2017-05-11 17:18:52 +0300

    first commit

---
+++ missing Number/missing_number.py
@@ -0,0 +1,13 @@
+def find_missing(list_one, list_two):
+    """find_missing function
+    find the missing number between two lists
+    """
+    # check if both lists are empty
+    if len(list_one) == len(list_two) == 0:
+        return 0
+
+    # check if both lists are similar
+    if not set(list_one).symmetric_difference(set(list_two)):
+        return 0
+    else:
+        return list(set(list_one).symmetric_difference(set(list_two)))[0]
